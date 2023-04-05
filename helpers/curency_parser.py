ca785c25895a1b5f4411b006a8f2fcc49a16fa15
Kratosterra <44316137+kratosterra@users.noreply.github.com>
2023-04-05 16:48:31 +0300

    Added currencies.json and saving currencies data for error situations in parsing

--- helpers/curency_parser.py
+++ helpers/curency_parser.py
@@ -1,7 +1,10 @@
+import json
+import logging
+
 import requests
 from bs4 import BeautifulSoup
 
-from config import currency
+from config.config import currency
 
 
 def get_exchange_rate(now_currency: str, new_currency: str) -> float:
@@ -22,15 +25,29 @@ def get_exchange_rate_dict(currency_str: str) -> dict:
     :param currency_str: Валюта в трехбуквенном строковом представлении
     :return: Словарь с обменным курсом для валют
     """
-    full_content = requests.get(f"https://www.x-rates.com/table/?from={currency_str}&amount=1").content
-    html_data = BeautifulSoup(full_content, "html.parser")
-    exchange_tables = html_data.find_all("table")
     exchange_rates = {}
-    for exchange_table in exchange_tables:
-        for tr in exchange_table.find_all("tr"):
-            tds = tr.find_all("td")
-            if tds:
-                currency_data = tds[0].text
-                exchange_rate = float(tds[1].text)
-                exchange_rates[currency_data] = exchange_rate
+    try:
+        full_content = requests.get(f"https://www.x-rates.com/table/?from={currency_str}&amount=1").content
+        html_data = BeautifulSoup(full_content, "html.parser")
+        exchange_tables = html_data.find_all("table")
+        for exchange_table in exchange_tables:
+            for tr in exchange_table.find_all("tr"):
+                tds = tr.find_all("td")
+                if tds:
+                    currency_data = tds[0].text
+                    exchange_rate = float(tds[1].text)
+                    exchange_rates[currency_data] = exchange_rate
+        # Сохранить данные
+        with open('../config/currencies.json', 'r') as f:
+            json_string = f.read()
+            data = json.loads(json_string)
+        data[currency_str] = exchange_rates
+        with open('../config/currencies.json', 'w') as f:
+            json.dump(data, f)
+    except Exception as e:
+        logging.error(f"{get_exchange_rate_dict.__name__}: {e}. Используем старые данные!")
+        with open('../config/currencies.json', 'r') as f:
+            json_string = f.read()
+            data = json.loads(json_string)
+        exchange_rates = data[currency_str]
     return exchange_rates
